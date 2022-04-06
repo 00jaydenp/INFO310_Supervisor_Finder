be446c379eeae975a46c548e65f0d668bab0ad36
davidc <davidcurrier11@gmail.com>
2022-04-07 11:46:38 +1200

    Created Supervisor domain class
    refs #1

--- INFO310_project/src/main/java/domain/Supervisor.java
+++ INFO310_project/src/main/java/domain/Supervisor.java
@@ -4,10 +4,87 @@
  */
 package domain;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  *
  * @author David
  */
 public class Supervisor {
     
+    private String staffID;
+    private String firstName;
+    private String lastName;
+    private String interests;
+    private String description;
+    private String phoneNumber;
+    
+    private User user;
+    private Collection<Project> projects = new ArrayList<>();
+
+    public String getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(String staffID) {
+        this.staffID = staffID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getInterests() {
+        return interests;
+    }
+
+    public void setInterests(String interests) {
+        this.interests = interests;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Collection<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Collection<Project> projects) {
+        this.projects = projects;
+    }      
+            
 }
