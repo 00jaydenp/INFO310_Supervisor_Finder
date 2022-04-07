f4b97b2238726a1a7b9fc477380115cb037cb188
phmci811 <phmci811@student.otago.ac.nz>
2022-04-07 17:01:33 +1200

    Added JDBIDaoFactory class

---
+++ INFO310_project/src/main/java/dao/JDBIDaoFactory.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
+/**
+ *
+ * @author phmci
+ */
+public class JDBIDaoFactory {
+
+    private static final String DB_USERNAME = "sa";
+    private static final String DB_PASSWORD = "sa";
+
+    private static String jdbcUri = "jdbc:h2:tcp://localhost/~/test"; //jdbc:h2:tcp://localhost/~/test
+
+    private static HikariDataSource HIKARI_DATA_SOURCE;
+    private static Jdbi JDBI;
+
+    public static void setJdbcUri(String uri) {
+        if (HIKARI_DATA_SOURCE != null) {
+            throw new IllegalStateException("Connection pool as already been initialised.  It is too late to change the JDBC URI.");
+        }
+
+        jdbcUri = uri;
+    }
+
+    private static void initialisePool() {
+        HIKARI_DATA_SOURCE = new HikariDataSource();
+        HIKARI_DATA_SOURCE.setJdbcUrl(jdbcUri);
+        HIKARI_DATA_SOURCE.setUsername(DB_USERNAME);
+        HIKARI_DATA_SOURCE.setPassword(DB_PASSWORD);
+
+        JDBI = Jdbi.create(HIKARI_DATA_SOURCE);
+        JDBI.installPlugin(new SqlObjectPlugin());
+    }
+}
