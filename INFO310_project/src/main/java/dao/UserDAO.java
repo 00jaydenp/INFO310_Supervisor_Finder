40024759e1e1d82518c78a2ab1a68cadf49f11ab
Francesca Totty <totfr473@student.otago.ac.nz>
2022-04-12 19:59:00 +1200

    Updated the deleteStudent and deleteSupervisor method - still need to test - close #21

--- INFO310_project/src/main/java/dao/UserDAO.java
+++ INFO310_project/src/main/java/dao/UserDAO.java
@@ -5,15 +5,78 @@
  */
 package dao;
 
-import domain.User;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Connection;
+import org.h2.jdbc.JdbcConnection;
+import domain.Student;
+import domain.Supervisor;
+
 /**
  *
  * @author francesca
  */
-public interface UserDAO {
+public class UserDAO implements StudentDao, SupervisorDAO {
     
-    void deleteUser(User user);
+    private final String studentUri = JDBIDaoFactory.getDefaultConnectionJdbcUri();
+    private final String supervisorUri = JDBIDaoFactory.getDefaultConnectionJdbcUri();
+  
+    public void deleteStudent(String Id){
+        String sql = "delete from student where studentID =?";
+        try(
+            Connection dbCon = JDBIDaoFactory.getConnection(studentUri);
+            PreparedStatement stmt = dbCon.prepareStatement(sql);
+                ){
+            stmt.executeUpdate();
+        }
+        catch (SQLException ex){
+            throw new RuntimeException(ex);
+        }
+    }
+   
+    public void deleteSupervisor(Supervisor supervisor){
+        String sql = "delete from supervisor where supervisorID =?";
+        try(
+            Connection dbCon = JDBIDaoFactory.getConnection(supervisorUri);
+            PreparedStatement stmt = dbCon.prepareStatement(sql);
+                ){
+            stmt.executeUpdate();
+        }
+        catch (SQLException ex){
+            throw new RuntimeException(ex);
+        }
+    }
     
-    void changePassword(User user);
+   // public void changePassword(User user){
+
+    @Override
+    public Student getByID(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void saveStudent(Student student) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Supervisor getSupervisorById(String staffId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addSupervisor(Supervisor supervisor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateSupervisor(String staffId, Supervisor supervisor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void hideSupervisor(String staffId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
