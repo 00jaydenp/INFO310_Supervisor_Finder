d4aeba7e63a8d447034a872cffee1025758fcc01
calvi <calvi@LAPTOP-DU6AM9BF.fritz.box>
2022-04-12 23:59:27 +1200

    Refs #16 - Started on resource file for supervisor

---
+++ INFO310_project/src/main/java/resource/SupervisorResource.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package resource;
+
+import dao.SupervisorDAO;
+import io.jooby.Jooby;
+
+/**
+ *
+ * @author calvi
+ */
+public class SupervisorResource extends Jooby{
+
+}
