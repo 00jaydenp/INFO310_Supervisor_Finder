25504acd4bab03ff561fbfc048802899e360405f
praja126 <praja126@student.otago.ac.nz>
2022-04-24 17:17:37 +1200

    Made default value for Hidden field so its not needed for insert statements

--- INFO310_project/src/main/java/dao/schema.sql
+++ INFO310_project/src/main/java/dao/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE Student(
     gpa varchar(10) not null, 
     address varchar(50) not null,
     email varchar(50) not null unique,
-    hidden boolean not null,
+    hidden boolean not null default 0,
     
     
     constraint Student_PK primary key (studentID),
@@ -30,7 +30,7 @@ CREATE TABLE Supervisor(
     description varchar(50) not null,
     phoneNumber varchar(20) not null,
     email varchar(50) not null unique,
-    hidden boolean not null,
+    hidden boolean not null default 0,
      
     
     constraint Supervisor_PK primary key (staffID),
@@ -44,7 +44,7 @@ CREATE TABLE Project(
     status varchar (10) not null,
     openDate date,
     studentID varchar(5) not null,
-    hidden boolean not null,
+    hidden boolean not null default 0,
 
     constraint Project_PK primary key (projectID),
     constraint ProjectStaffID_FK foreign key (staffID) references Supervisor(staffID),
