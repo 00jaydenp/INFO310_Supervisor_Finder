e96e682ca51ef028f887a6ce216b9d9d92b4165d
praja126 <praja126@student.otago.ac.nz>
2022-04-26 17:02:23 +1200

    Finished Supervisor Tests, all pass. Close #19

--- INFO310_project/src/test/java/dao/StudentCollectionsDaoTest.java
+++ INFO310_project/src/test/java/dao/StudentCollectionsDaoTest.java
@@ -4,71 +4,64 @@
  */
 package dao;
 
-import domain.Project;
 import domain.Student;
 import domain.User;
-import java.util.Collection;
+
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.*;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.BeforeAll;
 
-
 /**
  *
  * @author Jayden
  */
 public class StudentCollectionsDaoTest {
+
     StudentDao studentDao;
     UserDao userDao;
-    
-    
+
     private Student student1;
-    private Student student2; 
+    private Student student2;
     private Student student3;
     private User user1;
     private User user2;
     private User user3;
-    
-    
+
     @BeforeAll
-    public static void initialise(){
+    public static void initialise() {
         JDBIDaoFactory.setJdbcUri("jdbc:h2:mem:tests;INIT=runscript from 'src/main/java/dao/schema.sql'");
     }
-    
+
     @BeforeEach
     public void setUp() {
         studentDao = JDBIDaoFactory.getStudentDao();
         userDao = JDBIDaoFactory.getUserDao();
-        
+
         user1 = new User();
         user1.setEmail("smigr123@otago.ac.nz");
         user1.setPassword("mathsiscool23");
-        
+
         user2 = new User();
         user2.setEmail("jonbo234@otago.ac.nz");
         user2.setPassword("biologyrocks123");
-        
+
         user3 = new User();
         user3.setEmail("leest567@otago.ac.nz");
         user3.setPassword("physicsisfun123");
-        
+
         userDao.saveUser(user1);
         userDao.saveUser(user2);
         userDao.saveUser(user3);
-        
+
         student1 = new Student();
         student1.setStudentID("smigr123");
         student1.setFirstName("Greg");
@@ -79,7 +72,7 @@ public class StudentCollectionsDaoTest {
         student1.setGpa(3.1);
         student1.setAddress("123 George Street");
         student1.setUser(user1);
-                
+
         student2 = new Student();
         student2.setStudentID("jonbo234");
         student2.setFirstName("Bob");
@@ -90,7 +83,7 @@ public class StudentCollectionsDaoTest {
         student2.setGpa(4.0);
         student2.setAddress("123 Castle Street");
         student2.setUser(user2);
-        
+
         student3 = new Student();
         student3.setStudentID("leest567");
         student3.setFirstName("Stacy");
@@ -101,18 +94,17 @@ public class StudentCollectionsDaoTest {
         student3.setGpa(3.5);
         student3.setAddress("123 Grange Street");
         student3.setUser(user3);
-        
-        
+
         studentDao.saveStudent(student1);
         studentDao.saveStudent(student2);
     }
-    
+
     @AfterEach
     public void tearDown() {
         studentDao.deleteStudentByID(student1.getStudentID());
         studentDao.deleteStudentByID(student2.getStudentID());
         studentDao.deleteStudentByID(student3.getStudentID());
-        
+
         userDao.deleteUser(user1.getEmail());
         userDao.deleteUser(user2.getEmail());
         userDao.deleteUser(user3.getEmail());
@@ -129,13 +121,13 @@ public class StudentCollectionsDaoTest {
         assertThat(studentDao.getStudents(), hasSize(3));
         //Make sure it contains all student objects
         assertThat(studentDao.getStudents(), hasItems(student1, student2, student3));
-       
+
         //Make Duplicate student 
         final Student student4 = student3;
         // Try adding student4, make sure it throws exception due to duplicate values
         assertThrows(Exception.class, () -> {
-		studentDao.saveStudent(student4);
-	});
+            studentDao.saveStudent(student4);
+        });
 
     }
 
@@ -174,7 +166,6 @@ public class StudentCollectionsDaoTest {
         assertThat(studentDao.getStudents(), not(hasItem(student1)));
         //Should only be one student on record if succesful
         assertThat(studentDao.getStudents(), hasSize(1));
-     
 
     }
 
@@ -190,22 +181,21 @@ public class StudentCollectionsDaoTest {
         updateStudent.setLastName("Smith");
         updateStudent.setPhoneNumber("0223861135");
         updateStudent.setAddress("123 George Street");
-         updateStudent.setDescription("PhD Student Majoring in Mathematics");
+        updateStudent.setDescription("PhD Student Majoring in Mathematics");
         updateStudent.setInterests("Mathematics");
         updateStudent.setUser(user1);
         //update values
         updateStudent.setGpa(5.0);
-       
-       
+
         //Make sure its not the old value
         assertThat(student1.getGpa(), not(5.0));
         //Call update method
         studentDao.updateStudentByID(student1.getStudentID(), updateStudent);
-        
+
         //Make sure the details have updated, get new student
         Student newStudent = studentDao.getStudentByID(student1.getStudentID());
         assertThat(newStudent.getGpa(), is(5.0));
-        
+
     }
-    
+
 }
--- INFO310_project/src/test/java/dao/SupervisorCollectionsDaoTest.java
+++ INFO310_project/src/test/java/dao/SupervisorCollectionsDaoTest.java
@@ -5,98 +5,192 @@
 package dao;
 
 import domain.Supervisor;
-import java.util.Collection;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import domain.User;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeAll;
 
 /**
  *
- * @author David
+ * @author Jayden
  */
 public class SupervisorCollectionsDaoTest {
-    
-      
+
+    private SupervisorDao supervisorDao;
+    private UserDao userDao;
+
+    private User user1;
+    private User user2;
+    private User user3;
+
+    private Supervisor supervisor1;
+    private Supervisor supervisor2;
+    private Supervisor supervisor3;
+
+    @BeforeAll
+    public static void initialise() {
+        JDBIDaoFactory.setJdbcUri("jdbc:h2:mem:tests;INIT=runscript from 'src/main/java/dao/schema.sql'");
+    }
+
     @BeforeEach
     public void setUp() {
+
+        supervisorDao = JDBIDaoFactory.getSupervisorDao();
+        userDao = JDBIDaoFactory.getUserDao();
+
+        user1 = new User();
+        user1.setEmail("doejo222@otago.ac.nz");
+        user1.setPassword("Scienceisfun1");
+
+        user2 = new User();
+        user2.setEmail("doeja333@otago.ac.nz");
+        user2.setPassword("Musicislove2");
+
+        user3 = new User();
+        user3.setEmail("smibo444@otago.ac.nz");
+        user3.setPassword("Mathiscool3");
+
+        userDao.saveUser(user1);
+        userDao.saveUser(user2);
+        userDao.saveUser(user3);
+
+        supervisor1 = new Supervisor();
+        supervisor1.setStaffID("doejo222");
+        supervisor1.setFirstName("John");
+        supervisor1.setLastName("Doe");
+        supervisor1.setInterests("Space research and Mechanics");
+        supervisor1.setDescription("Senior lecturer in the Otago physics department");
+        supervisor1.setPhoneNumber("0275556667");
+        supervisor1.setUser(user1);
+
+        supervisor2 = new Supervisor();
+        supervisor2.setStaffID("doeja333");
+        supervisor2.setFirstName("Jane");
+        supervisor2.setLastName("Doe");
+        supervisor2.setInterests("Music and its influence on culture");
+        supervisor2.setDescription("First year lecturer in the Otago music department");
+        supervisor2.setPhoneNumber("0221112223");
+        supervisor2.setUser(user2);
+
+        supervisor3 = new Supervisor();
+        supervisor3.setStaffID("smibo444");
+        supervisor3.setFirstName("Bob");
+        supervisor3.setLastName("Smith");
+        supervisor3.setInterests("The inevitable heat death of the universe");
+        supervisor3.setDescription("Senior  lecturer in the Otago physics department");
+        supervisor3.setPhoneNumber("0212345678");
+        supervisor3.setUser(user3);
+
+        supervisorDao.saveSupervisor(supervisor1);
+        supervisorDao.saveSupervisor(supervisor2);
     }
-    
+
     @AfterEach
     public void tearDown() {
+        supervisorDao.deleteSupervisor(supervisor1.getStaffID());
+        supervisorDao.deleteSupervisor(supervisor2.getStaffID());
+        supervisorDao.deleteSupervisor(supervisor3.getStaffID());
+
+        userDao.deleteUser(user1.getEmail());
+        userDao.deleteUser(user2.getEmail());
+        userDao.deleteUser(user3.getEmail());
     }
 
     /**
-     * Test of getSupervisors method, of class SupervisorCollectionsDao.
+     * Test method to get all supervisors on record
      */
     @Test
     public void testGetSupervisors() {
-        System.out.println("getSupervisors");
-        SupervisorCollectionsDao instance = new SupervisorCollectionsDao();
-        Collection<Supervisor> expResult = null;
-        Collection<Supervisor> result = instance.getSupervisors();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        // Make sure it returns the two superviors on the database
+        assertThat(supervisorDao.getSupervisors(), hasItems(supervisor1, supervisor2));
+        //Make sure it only has those two
+        assertThat(supervisorDao.getSupervisors(), hasSize(2));
+
     }
 
     /**
-     * Test of getSupervisorById method, of class SupervisorCollectionsDao.
+     * Test method to get supervisor by their specific ID
      */
     @Test
     public void testGetSupervisorById() {
-        System.out.println("getSupervisorById");
-        String id = "";
-        SupervisorCollectionsDao instance = new SupervisorCollectionsDao();
-        Supervisor expResult = null;
-        Supervisor result = instance.getSupervisorById(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //Get the supervisor by their ID
+        Supervisor testSupervisor = supervisorDao.getSupervisorById(supervisor1.getStaffID());
+        //Make sure the returned supervisor is the one specified by the ID
+        assertThat(testSupervisor.getStaffID(), is(supervisor1.getStaffID()));
+        //Make sure the returned supervisor is not being mistakingly recognised
+        assertThat(testSupervisor.getStaffID(), not(is(supervisor2.getStaffID())));
+
     }
 
     /**
-     * Test of saveSupervisor method, of class SupervisorCollectionsDao.
+     * Test to add supervisor to record
      */
     @Test
     public void testSaveSupervisor() {
-        System.out.println("saveSupervisor");
-        Supervisor supervisor = null;
-        SupervisorCollectionsDao instance = new SupervisorCollectionsDao();
-        instance.saveSupervisor(supervisor);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //Add supervisor
+        supervisorDao.saveSupervisor(supervisor3);
+        //Check that there are now 3 supervisors in database
+        assertThat(supervisorDao.getSupervisors(), hasSize(3));
+        //Make sure it contains all supervisor objects
+        assertThat(supervisorDao.getSupervisors(), hasItems(supervisor1, supervisor2, supervisor3));
+
+        //Make Duplicate supervisor 
+        final Supervisor supervisor4 = supervisor3;
+        // Try adding supervisor4, make sure it throws exception due to duplicate values
+        assertThrows(Exception.class, () -> {
+            supervisorDao.saveSupervisor(supervisor4);
+        });
+
     }
 
     /**
-     * Test of updateSupervisor method, of class SupervisorCollectionsDao.
+     * Test to update existing record of a supervisor
      */
     @Test
     public void testUpdateSupervisor() {
-        System.out.println("updateSupervisor");
-        String id = "";
-        Supervisor supervisor = null;
-        SupervisorCollectionsDao instance = new SupervisorCollectionsDao();
-        instance.updateSupervisor(id, supervisor);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //Create a clone of Supervisor1 to update
+        Supervisor updatedSup = new Supervisor();
+        updatedSup.setStaffID("doejo222");
+        updatedSup.setFirstName("John");
+        updatedSup.setInterests("Space research and Mechanics");
+        updatedSup.setDescription("Senior lecturer in the Otago physics department");
+        updatedSup.setPhoneNumber("0275556667");
+        updatedSup.setUser(user1);
+        //update values
+        updatedSup.setLastName("Smith");
+
+        //Make sure its not the old value
+        assertThat(supervisor1.getLastName(), not(updatedSup.getLastName()));
+        //Call update method
+        supervisorDao.updateSupervisor(supervisor1.getStaffID(), updatedSup);
+
+        //Make sure the details have updated, get new supervisor
+        Supervisor newSup = supervisorDao.getSupervisorById(supervisor1.getStaffID());
+        assertThat(newSup.getLastName(), is(updatedSup.getLastName()));
+
     }
 
     /**
-     * Test of deleteSupervisor method, of class SupervisorCollectionsDao.
+     * Test to delete a supervisor from the record
      */
     @Test
     public void testDeleteSupervisor() {
-        System.out.println("deleteSupervisor");
-        String id = "";
-        SupervisorCollectionsDao instance = new SupervisorCollectionsDao();
-        instance.deleteSupervisor(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //Delete supervisor by their ID
+        supervisorDao.deleteSupervisor(supervisor1.getStaffID());
+        //Make sure the Staff is no longer on record
+        assertThat(supervisorDao.getSupervisors(), not(hasItem(supervisor1)));
+        //Should only be one Staff on record if succesful
+        assertThat(supervisorDao.getSupervisors(), hasSize(1));
+
     }
-    
+
 }
