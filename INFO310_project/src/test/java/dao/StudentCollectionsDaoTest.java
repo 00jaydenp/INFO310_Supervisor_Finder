6b769e3916a51511777c72e53e4991ffba784a2f
praja126 <praja126@student.otago.ac.nz>
2022-04-26 12:19:57 +1200

    Implemented SetUp and TearDown for StudentDAO test, Refs #19

--- INFO310_project/src/test/java/dao/StudentCollectionsDaoTest.java
+++ INFO310_project/src/test/java/dao/StudentCollectionsDaoTest.java
@@ -5,6 +5,7 @@
 package dao;
 
 import domain.Student;
+import domain.User;
 import java.util.Collection;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -20,14 +21,49 @@ import static org.junit.Assert.*;
  * @author David
  */
 public class StudentCollectionsDaoTest {
+    StudentDao dao = JDBIDaoFactory.getStudentDao();
     
-    
+    private Student student1;
+    private Student student2; 
+    private User user1;
+    private User user2;
     @BeforeEach
     public void setUp() {
+        user1 = new User();
+        user1.setEmail("gmsith@gmail.com");
+        user1.setPassword("smithg123");
+        
+        user2 = new User();
+        user2.setEmail("bjones@gmail.com");
+        user2.setPassword("jonesb123");
+        
+        student1 = new Student();
+        student1.setStudentID("1234");
+        student1.setFirstName("Greg");
+        student1.setLastName("Smith");
+        student1.setInterests("Mathematics");
+        student1.setDescription("PhD Student in Mathematics");
+        student1.setPhoneNumber("0223861135");
+        student1.setGpa(3.1);
+        student1.setAddress("123 George Street");
+        student1.setUser(user1);
+                
+        student2 = new Student();
+        student2.setStudentID("1235");
+        student2.setFirstName("Bob");
+        student2.setLastName("Jones");
+        student2.setInterests("Biology");
+        student2.setDescription("PhD Student in Biology");
+        student2.setPhoneNumber("0217432651");
+        student2.setGpa(4.0);
+        student2.setAddress("123 Castle Street");
+        student2.setUser(user2);
     }
     
     @AfterEach
     public void tearDown() {
+        dao.deleteStudentByID(student1.getStudentID());
+        dao.deleteStudentByID(student2.getStudentID());
     }
 
     /**
@@ -35,12 +71,7 @@ public class StudentCollectionsDaoTest {
      */
     @Test
     public void testSaveStudent() {
-        System.out.println("saveStudent");
-        Student student = null;
-        StudentCollectionsDao instance = new StudentCollectionsDao();
-        instance.saveStudent(student);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
     }
 
     /**
@@ -48,13 +79,8 @@ public class StudentCollectionsDaoTest {
      */
     @Test
     public void testGetStudents() {
-        System.out.println("getStudents");
-        StudentCollectionsDao instance = new StudentCollectionsDao();
-        Collection<Student> expResult = null;
-        Collection<Student> result = instance.getStudents();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+      
     }
 
     /**
@@ -62,14 +88,8 @@ public class StudentCollectionsDaoTest {
      */
     @Test
     public void testGetStudentByID() {
-        System.out.println("getStudentByID");
-        String id = "";
-        StudentCollectionsDao instance = new StudentCollectionsDao();
-        Student expResult = null;
-        Student result = instance.getStudentByID(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+      
     }
 
     /**
@@ -77,12 +97,7 @@ public class StudentCollectionsDaoTest {
      */
     @Test
     public void testDeleteStudentByID() {
-        System.out.println("deleteStudentByID");
-        String id = "";
-        StudentCollectionsDao instance = new StudentCollectionsDao();
-        instance.deleteStudentByID(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
     }
 
     /**
@@ -90,13 +105,7 @@ public class StudentCollectionsDaoTest {
      */
     @Test
     public void testUpdateStudentByID() {
-        System.out.println("updateStudentByID");
-        String id = "";
-        Student updatedStudent = null;
-        StudentCollectionsDao instance = new StudentCollectionsDao();
-        instance.updateStudentByID(id, updatedStudent);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
     }
     
 }
