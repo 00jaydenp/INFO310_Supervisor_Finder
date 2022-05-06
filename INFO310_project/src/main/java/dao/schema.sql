6cbda73800f92c53c49cebb46fda41f583ef81d3
praja126 <praja126@student.otago.ac.nz>
2022-05-06 21:41:00 +1200

    Added Application table to schema

--- INFO310_project/src/main/java/dao/schema.sql
+++ INFO310_project/src/main/java/dao/schema.sql
@@ -1,8 +1,9 @@
-
+DROP TABLE application;
 DROP TABLE project;
 DROP TABLE supervisor;
 DROP TABLE student;
 DROP TABLE sysUser;
+
 
 CREATE TABLE sysUser(
     email varchar(50) not null unique,
@@ -44,6 +45,8 @@ CREATE TABLE Supervisor(
 );
 
 
+
+
 CREATE TABLE Project(
     projectID varchar(5) not null unique,
     name varchar(255) not null,
@@ -51,7 +54,7 @@ CREATE TABLE Project(
     status varchar (255) not null,
     date varchar(10),
     staffID varchar(8) not null,
-    studentID varchar(8),
+    studentID varchar(8) not null unique,
     hidden boolean not null default 0,
 
     constraint Project_PK primary key (projectID), 
@@ -60,3 +63,17 @@ CREATE TABLE Project(
     constraint StudentID_FK foreign key (studentID) references Student (studentID)
         on delete cascade
 );
+
+CREATE TABLE Application(
+    applicationID varchar(8) not null unique,
+    projectID varchar(5) not null unique,
+    studentID varchar(8) not null unique,
+
+    constraint Application_PK primary key (applicationID),
+    constraint Project_FK foreign key (projectID) references Project(projectID)
+        on delete cascade,
+    constraint Student_FK foreign key (studentID) references Student(studentID)
+        on delete cascade
+);
+
+
