b0bdcc347eff000c940c8bcd103bfc44553141f3
davidc <davidcurrier11@gmail.com>
2022-05-11 13:36:34 +1200

    Added Css for web service

--- INFO310_project/static/js/navigation.js
+++ INFO310_project/static/js/navigation.js
@@ -33,20 +33,20 @@ export const NavigationMenu = {
     template:
             `
     <nav>
-        <div v-if="studentSignedIn && signedIn">Welcome back {{studentuser.firstName}}</div>
-        <div v-if="supervisorSignedIn && signedIn">Welcome back {{supervisoruser.firstName}}</div>
-        <a href=".">Home</a>&nbsp
-        <a href ="login.html" v-if="!signedIn">Log In</a>&nbsp
-        <a href ="signup.html" v-if="!signedIn">Sign up</a>&nbsp
-        <a href ="studentlist.html" v-if="signedIn && supervisorSignedIn">Student List</a>&nbsp
-        <a href ="supervisorlookup.html" v-if="signedIn && studentSignedIn">Supervisor List</a>&nbsp
-        <a href="studentprofile.html" v-if="studentSignedIn && signedIn">Profile</a>&nbsp
-        <a href="supervisorprofile.html" v-if="supervisorSignedIn && signedIn">Profile</a>&nbsp
-        <a href="project-list.html" v-if="studentSignedIn && signedIn">Project</a>&nbsp
-        <a href="viewstudentapplications.html" v-if="studentSignedIn && signedIn">Applications</a>&nbsp
-        <a href="myproject.html" v-if="supervisorSignedIn && signedIn">My Project</a>&nbsp
-        <a href="addproject.html" v-if="supervisorSignedIn && signedIn">Add Project</a>&nbsp
-        <a href="#" v-if="signedIn" @click="signOut()">Sign Out</a>&nbsp
+        <div class="message" v-if="studentSignedIn && signedIn">Welcome back {{studentuser.firstName}}</div>
+        <div class="message" v-if="supervisorSignedIn && signedIn">Welcome back {{supervisoruser.firstName}}</div>
+        <a class="nav_button" href=".">Home</a>&nbsp
+        <a class="nav_button" href ="login.html" v-if="!signedIn">Log In</a>&nbsp
+        <a class="nav_button" href ="signup.html" v-if="!signedIn">Sign up</a>&nbsp
+        <a class="nav_button" href ="studentlist.html" v-if="signedIn && supervisorSignedIn">Student List</a>&nbsp
+        <a class="nav_button" href ="supervisorlookup.html" v-if="signedIn && studentSignedIn">Supervisor List</a>&nbsp
+        <a class="nav_button" href="studentprofile.html" v-if="studentSignedIn && signedIn">Profile</a>&nbsp
+        <a class="nav_button" href="supervisorprofile.html" v-if="supervisorSignedIn && signedIn">Profile</a>&nbsp
+        <a class="nav_button" href="project-list.html" v-if="studentSignedIn && signedIn">Project</a>&nbsp
+        <a class="nav_button" href="viewstudentapplications.html" v-if="studentSignedIn && signedIn">Applications</a>&nbsp
+        <a class="nav_button" href="myproject.html" v-if="supervisorSignedIn && signedIn">My Project</a>&nbsp
+        <a class="nav_button" href="addproject.html" v-if="supervisorSignedIn && signedIn">Add Project</a>&nbsp
+        <a class="nav_button" href="#" v-if="signedIn" @click="signOut()">Sign Out</a>&nbsp
     </nav>
     `,
 
